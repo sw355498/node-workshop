9110c4a325e0d9449de1687e80ed498b63e07b27
sw355498 <sw355498@gmail.com>
2021-08-01 10:26:46 +0800

    新增function sum1 ＆ function sum2 並加入結果顯示

--- basic/sum.js
+++ basic/sum.js
@@ -1,12 +1,25 @@
 console.log("Hello!")
-
-function sum(n) {
+/* 第一種寫法：for迴圈 */
+function sum(x) {
     let result = 0;
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= x; i++) {
         result += i;
     }
     return result;
 }
-console.log(sum(1))
-console.log(sum(3))
-console.log(sum(10))
+
+
+/* 第二種寫法：return搭配梯形面積的公式 */
+function sum1(y){
+    let resuult1 = 0;
+    resuult1 = ((1+y)*y)/2;
+    return resuult1
+}
+/* 第二種方法更好的寫法;不宣告變數直接使用return 執行 梯形面積的公式 */
+function sum2(z){
+    return ((1+z)*z)/2;
+}
+
+console.log("方法一結果：",sum(1),";方法二結果：",sum1(1),sum2(1))
+console.log("方法一結果：",sum(3),";方法二結果：",sum1(3),sum2(3))
+console.log("方法一結果：",sum(10),";方法二結果：",sum1(10),sum2(10))
