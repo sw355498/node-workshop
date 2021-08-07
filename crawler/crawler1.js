0dcea8747e91c41b18c3679495caca449d681202
sw355498 <sw355498@gmail.com>
2021-08-07 20:18:32 +0800

    更改為callback版本

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -3,14 +3,19 @@
 const axios = require("axios");
 const moment = require("moment");
 
-axios
+const fs = require("fs");
+let stockCode = ""
+fs.readFile("stock.txt", "utf-8", (err, stockCode) =>{
+  axios
   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
     params:{
       response: "json",
       date: moment().format("YYYYMMDD"),
-      stockNo: "2330",
+      stockNo: stockCode,
     },
   })
-.then((response) => {
+  .then((response) => {
     console.log(response.data);
+  })
 })
+
