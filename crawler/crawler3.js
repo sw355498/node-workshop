1d59212c4b34b5d59d218a04b6036b6d80d97ac5
sw355498 <sw355498@gmail.com>
2021-08-08 10:30:50 +0800

    新增crawler3.js將crawler2.js更改為await版本

---
+++ crawler/crawler3.js
@@ -0,0 +1,72 @@
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210807&stockNo=2330&_=1628308672978
+/* await版 第一種寫法 */
+const axios = require("axios");
+const moment = require("moment");
+
+const fs = require("fs");
+const { resolve } = require("path");
+let stockCode = ""
+
+async function resultStock(){
+  let stockCode = await new Promise ((resolve,reject) => {
+    fs.readFile("stock.txt", "utf-8", (err, stockCode) =>{
+      if (err){
+          reject(err);
+      }
+      else{
+          resolve(stockCode);
+      }
+    })
+  })
+    try{
+      let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+          params:{
+            response: "json",
+            date: moment().format("YYYYMMDD"),
+            stockNo: stockCode,
+          },
+      });  
+      console.log(result.data);
+    }
+    catch(e){
+      console.log(e);
+    }
+}
+resultStock()
+
+
+/* await版 第二種寫法 */
+// const axios = require("axios");
+// const moment = require("moment");
+// const fs = require("fs");
+
+// function resultPromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (error, stockCode) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(stockCode);
+//       }
+//     });
+//   });
+// }
+// function resultAxios(stockCode) {
+//   return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//     params: {
+//       response: "json",
+//       date: moment().format("YYYYMMDD"),
+//       stockNo: stockCode,
+//     },
+//   });
+// }
+
+// (async function () {
+//   try {
+//     let stockCode = await resultPromise();
+//     let response = await resultAxios(stockCode);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
