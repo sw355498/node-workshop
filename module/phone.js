6a965590a1c4b6788b78a3df062855392a345a40
詹皓丞 <sw355498@gmail.com>
2021-08-14 21:09:49 +0800

    新增phone.js模組

---
+++ module/phone.js
@@ -0,0 +1,39 @@
+let brand = 'iphon'
+let model = 'iPhone 12 Pro'
+let size = '6.7吋'
+let color = '太平洋藍'
+let capacity = '256GB'
+let price = '41400'
+
+function showBrand() {
+  return brand
+}
+
+function showModel() {
+  return model
+}
+
+function showSize() {
+  return size
+}
+
+function showColor() {
+  return color
+}
+
+function showCapacity() {
+  return capacity
+}
+
+function showPrice() {
+  return price
+}
+
+module.exports = {
+    showBrand,
+    showModel,
+    showSize,
+    showColor,
+    showCapacity,
+    showPrice,
+  }
