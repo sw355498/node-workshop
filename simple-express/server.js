db7633772ede19ee5bb1611457f7edf13117c347
詹皓丞 <sw355498@gmail.com>
2021-08-15 10:38:53 +0800

    新增 package & 新增server.js並撰寫內容

---
+++ simple-express/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// 利用 express 建立了一個 express application
+let app = express();
+
+// HTTP Method: get, post, put, patch, delete
+app.get("/", function (request, response, next) {
+  response.send("Hello");
+});
+
+app.get("/about", function (request, response, next) {
+  response.send("這裡是about");
+});
+
+app.listen(3000, function () {
+  console.log("我們的 web server 啟動了～");
+});
+
