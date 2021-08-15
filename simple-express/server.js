46a7b2e385a1bd1ba411a59298526a31aa2b73c5
詹皓丞 <sw355498@gmail.com>
2021-08-15 11:30:36 +0800

    新增內容：中間件

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,7 +3,20 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+// app.use 使用一個中間件
+app.use((req, res ,next) => {
+  let current = new Date();
+  console.log(`訪客已經入，登入時間 ${current.toISOString()}`)
+  next()
+})
+
+app.use((req, res ,next) => {
+  console.log("訪客使用中")
+  next()
+})
+
 // HTTP Method: get, post, put, patch, delete
+// router 路由 -> 特殊的中間件會去比對網址
 app.get("/", function (request, response, next) {
   response.send("Hello");
 });
