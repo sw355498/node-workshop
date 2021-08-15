950ff1c1391daf960793c0c1cf5f9e00a9acfb33
詹皓丞 <sw355498@gmail.com>
2021-08-15 10:10:14 +0800

    更改檔案內容

--- crawler/crawlerdb3.js
+++ crawler/crawlerdb3.js
@@ -1,66 +1,55 @@
-const axios = require("axios");
-const moment = require("moment");
-//fs內建讀檔案,不用npm,使用promises版
+// 直接用 fs 的 promises 版本
 const fs = require("fs/promises");
 const connection = require("./utils/db");
+const axios = require("axios");
 const { processStockDay } = require("./utils/TWSEDataProcessor");
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-
+const moment = require("moment");
 
 (async () => {
   try {
-    // 1.讀 stock.txt 把股票代碼讀進來
+    // 1. 讀 stock.txt 把股票代碼讀進來
     let stockCode = await fs.readFile("stock.txt", "utf8");
+    // console.log(stockCode);
 
-    //console.log(stockCode);
-
-    // 2.去資料庫的 stock 表格查看看，這個代碼是不是在我們的服務範圍內
-    //await 等他連接好
+    // 2. 去資料庫的 stock 表格查看看，這個代碼是不是在我們的服務範圍內
     await connection.connectAsync();
     let dbResults = await connection.queryAsync(
       "SELECT * FROM stock WHERE stock_id = ?",
       [stockCode]
     );
-    //console.log(dbResults);
-    if (dbResults === 0) {
-      throw "此代碼不在我們的服務範圍內";
+    console.log(dbResults);
+
+    if (dbResults.length === 0) {
+      throw "不在我們的服務範圍內";
     }
-    // 3.如果是，才去證交所抓資料
+
+    // 職責切割
+    // 3. 如果是，才去證交所抓資料
     let response = await axios.get(
       "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
       {
         params: {
           response: "json",
           date: moment().format("YYYYMMDD"),
-          stockNo: stockCode,
+          stockNo: stockCode, // 長榮航空 2618  長榮航海王 2603
         },
       }
     );
-      //console.log(response)
     const twseData = response.data;
     if (twseData.stat !== "OK") {
-      throw "從證交所查到的資料有問題";
+      throw "從證交所查到的資料有問題!";
     }
-    let parseData = processStockDay(stockCode, twseData.data);
+    let parsedData = processStockDay(stockCode, twseData.data);
 
-    // 4.抓回來的資料存到資料庫的 stock_price 表格裡去
+    // 4. 抓回來的資料存到資料庫的 stock_price 表格裡去
     let results = await connection.queryAsync(
-        "INSERT  IGNORE into stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) values ? ",
-        [parseData]
-      );
-      console.log(results)
+      "INSERT IGNORE INTO stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUES ?",
+      [parsedData]
+    );
+    console.log(results);
   } catch (e) {
     console.error(e);
   } finally {
-    //關閉連線
     connection.end();
   }
 })();
